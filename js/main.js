4229a22b30c3df9f45cf41e7e2bb8a002639cc57
LisVM <vladimkazakov@yandex.ru>
2020-08-30 21:58:19 +0300

    changes_4.7

--- js/main.js
+++ js/main.js
@@ -11,6 +11,16 @@
 
 // Появление информации об услугах
 
-function FuncImages() {
-  document.getElementById("main").style.display = "block";}
-  //document.getElementById("main").style.display = "none";
+let clickEl = document.getElementById('click');
+clickEl.addEventListener('click', function(){
+  document.getElementById('main').style.display = 'block';
+})
+document.querySelector('#main').setAttribute('style','position: absolute !important; top:5rem !important; background: #5cb683 !important; border-radius: 20px !important');
+
+
+//Кнопка "Закрыть"
+
+let clickEl_2 = document.getElementById('button-close');
+clickEl_2.addEventListener('click', function(){
+  document.getElementById('main').style.display = 'none';
+})
