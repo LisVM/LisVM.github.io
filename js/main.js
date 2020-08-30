bc5544fe50a994a09a08d910e7a56f1a03141144
LisVM <vladimkazakov@yandex.ru>
2020-08-30 18:28:39 +0300

    changes_4.7

--- js/main.js
+++ js/main.js
@@ -7,3 +7,18 @@
   
   
   //# sourceURL=coffeescript
+
+
+// Появление информации об услугах
+  let clickElem_1 = document.getElementById('click');
+clickElem_1.addEventListener('click',function(e){
+    document.getElementById('content').style.display = 'block';
+
+//   let clickElem_2 = document.getElementById('click');
+// clickElem_2.addEventListener('click',function(e){
+//     document.getElementById('content').style.display = 'none';
+// })
+
+// let clickElem_2 = document.getElementById('content');
+// clickElem_2.addEventListener('click',function(e){
+//     document.getElementById('content').style.display = 'none';
