a4201f367d67fa0336aa470a81ea14500eadc8a5
LisVM <vladimkazakov@yandex.ru>
2020-08-31 14:39:20 +0300

    changes_5.0

--- js/main.js
+++ js/main.js
@@ -9,18 +9,83 @@
   //# sourceURL=coffeescript
 
 
-// Появление информации об услугах
+// Появление информации об услугах авиаперевозки грузов
 
-let clickEl = document.getElementById('click');
-clickEl.addEventListener('click', function(){
-  document.getElementById('main').style.display = 'block';
+let clickEl_1 = document.getElementById('click_1');
+clickEl_1.addEventListener('click', function(){
+  document.getElementById('main_1').style.display = 'block';
 })
-document.querySelector('#main').setAttribute('style','position: absolute !important; top:5rem !important; background: #5cb683 !important; border-radius: 20px !important');
+document.querySelector('#main_1').setAttribute('style','position: absolute !important; top:5rem !important; margin: auto !important; background: #5cb683 !important; border-radius: 20px !important; padding: 1.5rem !important');
 
 
 //Кнопка "Закрыть"
 
 let clickEl_2 = document.getElementById('button-close');
 clickEl_2.addEventListener('click', function(){
-  document.getElementById('main').style.display = 'none';
+  document.getElementById('main_1').style.display = 'none';
+})
+
+
+// Появление информации об услугах таможенное оформление
+
+let clickEl_3 = document.getElementById('click_2');
+clickEl_3.addEventListener('click', function(){
+  document.getElementById('main_2').style.display = 'block';
+})
+document.querySelector('#main_2').setAttribute('style','position: absolute !important; top:5rem !important; margin: auto !important; background: #5cb683 !important; border-radius: 20px !important; padding: 1.5rem !important');
+
+
+//Кнопка "Закрыть"
+
+let clickEl_4 = document.getElementById('button-close');
+clickEl_4.addEventListener('click', function(){
+  document.getElementById('main_2').style.display = 'none';
+})
+
+// Появление информации об услугах электронное декларирование
+
+let clickEl_5 = document.getElementById('click_3');
+clickEl_5.addEventListener('click', function(){
+  document.getElementById('main_3').style.display = 'block';
+})
+  document.querySelector('#main_3').setAttribute('style','position: absolute !important; top:5rem !important; margin: auto !important; background: #5cb683 !important; border-radius: 20px !important; padding: 1.5rem !important');
+
+
+//Кнопка "Закрыть"
+
+let clickEl_6 = document.getElementById('button-close');
+clickEl_6.addEventListener('click', function(){
+  document.getElementById('main_3').style.display = 'none';
+})
+
+// Появление информации об услугах складские услуги
+
+let clickEl_7 = document.getElementById('click_4');
+clickEl_7.addEventListener('click', function(){
+  document.getElementById('main_4').style.display = 'block';
+})
+  document.querySelector('#main_4').setAttribute('style','position: absolute !important; top:5rem !important; margin: auto !important; background: #5cb683 !important; border-radius: 20px !important; padding: 1.5rem !important');
+
+
+//Кнопка "Закрыть"
+
+let clickEl_8 = document.getElementById('button-close');
+clickEl_8.addEventListener('click', function(){
+  document.getElementById('main_4').style.display = 'none';
+})
+
+// Появление информации об услугах экспедиторские услуги
+
+let clickEl_9 = document.getElementById('click_5');
+clickEl_9.addEventListener('click', function(){
+  document.getElementById('main_5').style.display = 'block';
+})
+document.querySelector('#main_5').setAttribute('style','position: absolute !important; top:5rem !important; margin: auto !important; background: #5cb683 !important; border-radius: 20px !important; padding: 1.5rem !important');
+
+
+//Кнопка "Закрыть"
+
+let clickEl_10 = document.getElementById('button-close');
+clickEl_10.addEventListener('click', function(){
+  document.getElementById('main_5').style.display = 'none';
 })
