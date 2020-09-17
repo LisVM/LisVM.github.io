ccba8cc23e6a840c5b4214184979aa1dde3250fa
LisVM <vladimkazakov@yandex.ru>
2020-09-17 22:12:26 +0300

    changes_6.1

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
   //# sourceURL=coffeescript
 
 
-// Появление информации об услугах авиаперевозки грузов
+// Появление информации об услугах МЕЖДУНАРОДНЫЕ ПЕРЕВОЗКИ
 
 let clickEl_1 = document.getElementById('click_1');
 clickEl_1.addEventListener('click', function(){
@@ -26,7 +26,7 @@ clickEl_2.addEventListener('click', function(){
 })
 
 
-// Появление информации об услугах таможенное оформление
+// Появление информации об услугах ВНУТРИРОССИЙСКИЕ ПЕРЕВОЗКИ
 
 let clickEl_3 = document.getElementById('click_2');
 clickEl_3.addEventListener('click', function(){
@@ -42,7 +42,7 @@ clickEl_4.addEventListener('click', function(){
   document.getElementById('main_2').style.display = 'none';
 })
 
-// Появление информации об услугах электронное декларирование
+// Появление информации об услугах ПЕРЕВОЗКА НЕГАБАРИТНЫХ ГРУЗОВ
 
 let clickEl_5 = document.getElementById('click_3');
 clickEl_5.addEventListener('click', function(){
@@ -58,7 +58,7 @@ clickEl_6.addEventListener('click', function(){
   document.getElementById('main_3').style.display = 'none';
 })
 
-// Появление информации об услугах складские услуги
+// Появление информации об услугах СБОРНЫЙ ГРУЗ
 
 let clickEl_7 = document.getElementById('click_4');
 clickEl_7.addEventListener('click', function(){
@@ -74,7 +74,7 @@ clickEl_8.addEventListener('click', function(){
   document.getElementById('main_4').style.display = 'none';
 })
 
-// Появление информации об услугах экспедиторские услуги
+// Появление информации об услугах ЭКСПРЕСС ГРУЗЫ, МПО
 
 let clickEl_9 = document.getElementById('click_5');
 clickEl_9.addEventListener('click', function(){
@@ -88,4 +88,84 @@ document.querySelector('#main_5').setAttribute('style','position: absolute !impo
 let clickEl_10 = document.getElementById('button-close_5');
 clickEl_10.addEventListener('click', function(){
   document.getElementById('main_5').style.display = 'none';
+})
+
+// Появление информации об услугах ТАМОЖЕННОЕ ОФОРМЛЕНИЕ
+
+let clickEl_11 = document.getElementById('click_6');
+clickEl_11.addEventListener('click', function(){
+  document.getElementById('main_6').style.display = 'block';
+})
+document.querySelector('#main_6').setAttribute('style','position: absolute !important; top:5rem !important; margin: auto !important; background: #5cb683 !important; border-radius: 20px !important; padding: 1.5rem !important');
+
+
+//Кнопка "Закрыть"
+
+let clickEl_12 = document.getElementById('button-close_6');
+clickEl_12.addEventListener('click', function(){
+  document.getElementById('main_6').style.display = 'none';
+})
+
+// Появление информации об услугах РАЗРЕШИТЕЛЬНАЯ ДОКУМЕНТАЦИЯ
+
+let clickEl_13 = document.getElementById('click_7');
+clickEl_13.addEventListener('click', function(){
+  document.getElementById('main_7').style.display = 'block';
+})
+document.querySelector('#main_7').setAttribute('style','position: absolute !important; top:5rem !important; margin: auto !important; background: #5cb683 !important; border-radius: 20px !important; padding: 1.5rem !important');
+
+
+//Кнопка "Закрыть"
+
+let clickEl_14 = document.getElementById('button-close_7');
+clickEl_14.addEventListener('click', function(){
+  document.getElementById('main_7').style.display = 'none';
+})
+
+// Появление информации об услугах СКЛАДСКИЕ УСЛУГИ
+
+let clickEl_15 = document.getElementById('click_8');
+clickEl_15.addEventListener('click', function(){
+  document.getElementById('main_8').style.display = 'block';
+})
+document.querySelector('#main_8').setAttribute('style','position: absolute !important; top:5rem !important; margin: auto !important; background: #5cb683 !important; border-radius: 20px !important; padding: 1.5rem !important');
+
+
+//Кнопка "Закрыть"
+
+let clickEl_16 = document.getElementById('button-close_8');
+clickEl_16.addEventListener('click', function(){
+  document.getElementById('main_8').style.display = 'none';
+})
+
+// Появление информации об услугах СТРАХОВАНИЕ ГРУЗА
+
+let clickEl_17 = document.getElementById('click_9');
+clickEl_17.addEventListener('click', function(){
+  document.getElementById('main_9').style.display = 'block';
+})
+document.querySelector('#main_9').setAttribute('style','position: absolute !important; top:5rem !important; margin: auto !important; background: #5cb683 !important; border-radius: 20px !important; padding: 1.5rem !important');
+
+
+//Кнопка "Закрыть"
+
+let clickEl_18 = document.getElementById('button-close_9');
+clickEl_18.addEventListener('click', function(){
+  document.getElementById('main_9').style.display = 'none';
+})
+
+// Появление информации об услугах КОНСУЛЬТАЦИИ ПО ВОПРОСАМ ВЭД
+
+let clickEl_19 = document.getElementById('click_10');
+clickEl_19.addEventListener('click', function(){
+  document.getElementById('main_10').style.display = 'block';
+})
+document.querySelector('#main_10').setAttribute('style','position: absolute !important; top:5rem !important; margin: auto !important; background: #5cb683 !important; border-radius: 20px !important; padding: 1.5rem !important');
+
+
+//Кнопка "Закрыть"
+
+let clickEl_20 = document.getElementById('button-close_10');
+clickEl_20.addEventListener('click', function(){
+  document.getElementById('main_10').style.display = 'none';
 })
